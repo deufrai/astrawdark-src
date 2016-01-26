c67871900d86fffed07471d874e153b374b4e638
Frederic CORNU <fcornu@wardsback.org>
2016-01-26 22:07:03 +0100

    [ARD-104] update file licence and class brief for MatchDarksCommand

--- src/commands/matchDarksCommand.h
+++ src/commands/matchDarksCommand.h
@@ -1,8 +1,20 @@
 /*
- * matchDarksCommand.h
+ *  AstRawDark
  *
- *  Created on: 21 janv. 2016
- *      Author: deufrai
+ *  Copyright (C) 2015 - Frédéric CORNU
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef SRC_COMMANDS_MATCHDARKSCOMMAND_H_
@@ -12,9 +24,8 @@
 
 /**
  * @brief Command that creates a list of darks
- * from a list of lights
+ * from a list of lights.
  *
- * match T° distribution as close as possible between the 2 image lists
  */
 class MatchDarksCommand : public AbstractCommand {
 
